49f24377e4f1f89de47fc88654b4bb13a21535b6
Glen Sale and Kenny Wolfenberger <glen@>
2018-11-28 16:40:51 -0800

    adds categories

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,12 +1,19 @@
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  public class ItemTest : IDisposable
   {
-    // [TestMethod]
+        public void Dispose()
+    {
+        Item.ClearAll();
+    }
+    //     [TestMethod]
     // public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
     // {
     //   //Arrange
@@ -20,21 +27,21 @@ namespace ToDoList.Tests
     //   Assert.AreEqual(1, result);
     // }
 
-    [TestMethod]
-    public void Find_ReturnsCorrectItem_Item()
-    {
-      //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
+    // [TestMethod]
+    // public void Find_ReturnsCorrectItem_Item()
+    // {
+    //   //Arrange
+    //   string description01 = "Walk the dog";
+    //   string description02 = "Wash the dishes";
+    //   Item newItem1 = new Item(description01);
+    //   Item newItem2 = new Item(description02);
 
-      //Act
-      Item result = Item.Find(2);
+    //   //Act
+    //   Item result = Item.Find(2);
 
-      //Assert
-      Assert.AreEqual(newItem2, result);
-    }
+    //   //Assert
+    //   Assert.AreEqual(newItem2, result);
+    // }
 
   }
 }
